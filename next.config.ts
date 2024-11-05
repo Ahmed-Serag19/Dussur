ef92b896bab50b4f01641883345a20e952d79248
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-11-05 09:21:37 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        port: "",
+        pathname: "/account123/**",
+      },
+    ],
+  },
+};
+module.exports = {
+  images: {
+    domains: ["aceternity.com"],
+  },
+};
 
 export default nextConfig;
